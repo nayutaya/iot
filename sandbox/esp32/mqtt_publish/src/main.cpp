b5c80ef7cb30d7e895dbcea31d71b3f7e282c37f
Yuya Kato <yuyakato@gmail.com>
2019-01-28 01:13:55 +0900

    WiFiの接続処理を追加

--- sandbox/esp32/mqtt_publish/src/main.cpp
+++ sandbox/esp32/mqtt_publish/src/main.cpp
@@ -1,7 +1,25 @@
 
 #include <Arduino.h>
+#include <WiFi.h>
+
+#include <PubSubClient.h>
+
+#include "config.h"
 
 void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  Serial.println("...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while ( WiFi.waitForConnectResult() != WL_CONNECTED ) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
 }
 
 void loop() {
