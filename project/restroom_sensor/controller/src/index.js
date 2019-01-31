ca87be713c192d24d7f33a628e107bee9cde28c7
Yuya Kato <yuyakato@gmail.com>
2019-02-01 00:39:48 +0900

    トピックをサブスクライブする処理を追加

--- project/restroom_sensor/controller/src/index.js
+++ project/restroom_sensor/controller/src/index.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+const mqtt = require("mqtt");
+
+// TODO: MQTTサーバのアドレスを環境変数から取得する。
+const client = mqtt.connect("mqtt://127.0.0.1");
+
+const NOTIFICATION_TOPIC = "sensor/restroom/raw/notification";
+
+client.on("connect", () => {
+  console.log("connect");
+
+  client.subscribe(NOTIFICATION_TOPIC, (err) => {
+    console.log("subscribe");
+  });
+});
+
+client.on("message", (topic, message) => {
+  if ( topic === NOTIFICATION_TOPIC ) {
+    console.log("message:", [topic, JSON.parse(message)]);
+  }
+});
