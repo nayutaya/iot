7529410d2d37bf538a86ec01940802b1013b7f52
Yuya Kato <yuyakato@gmail.com>
2019-02-01 01:33:38 +0900

    RxJSを使って状態を生成するように変更

--- project/restroom_sensor/controller/src/index.js
+++ project/restroom_sensor/controller/src/index.js
@@ -2,12 +2,20 @@
 
 const mqtt = require("mqtt");
 
+const Rx = require("rxjs");
+const {
+  filter,
+  scan,
+} = require("rxjs/operators");
+
 // TODO: MQTTサーバのアドレスを環境変数から取得する。
 const client = mqtt.connect("mqtt://127.0.0.1");
 
 const NOTIFICATION_TOPIC = "sensor/restroom/raw/notification";
 const CONTROL_TOPIC      = "sensor/restroom/raw/control";
-
+const LIGHT_SENSOR_THRESHOLD = 1024;
+const COLOR_BUSY = {Red: 255, Green: 0, Blue: 0};
+const COLOR_FREE = {Red: 0, Green: 255, Blue: 0};
 
 client.on("connect", () => {
   console.log("[MQTT] connect");
@@ -17,24 +25,57 @@ client.on("connect", () => {
   });
 });
 
+const notificationMessageSubject = new Rx.BehaviorSubject(null);
 client.on("message", (topic, message) => {
   if ( topic === NOTIFICATION_TOPIC ) {
     const notificationMessage = JSON.parse(message);
     console.log("[MQTT] notificationMessage:", notificationMessage);
-
-    // TODO: 閾値を定数化する。
-    var color;
-    if ( notificationMessage.LightSensorValue >= 1024 ) {
-      color = {Red: 255, Green: 0, Blue: 0};
-    } else {
-      color = {Red: 0, Green: 255, Blue: 0};
-    }
-
-    const controlMessage = JSON.stringify({
-      Command: "SET_LED",
-      Color: color,
-    });
-    client.publish(CONTROL_TOPIC, controlMessage);
-    console.log("[MQTT] controlMessage:", controlMessage);
+    notificationMessageSubject.next(notificationMessage);
   }
+});
+
+const initialState = {
+  LightSensorValue: 0,
+  NumberOfPyroelectricSensorInterrupts: 0,
+  CurrentTime: 0,
+  IsLightOn: false,
+  LastLightTurnedOnTime: 0,
+  LastLightTurnedOffTime: 0,
+  LastDetectedHumanTime: 0,
+};
+const stateStream = notificationMessageSubject
+  .pipe(
+    filter((message) => message !== null),
+    scan((previousState, message) => {
+      const currentState = Object.assign({}, previousState);
+      currentState.LightSensorValue                     = message.LightSensorValue;
+      currentState.NumberOfPyroelectricSensorInterrupts = message.NumberOfPyroelectricSensorInterrupts;
+      currentState.CurrentTime                          = new Date().getTime();
+      currentState.IsLightOn                            = (currentState.LightSensorValue >= LIGHT_SENSOR_THRESHOLD);
+
+      if ( currentState.IsLightOn && !previousState.IsLightOn ) {
+        currentState.LastLightTurnedOnTime = currentState.CurrentTime;
+      }
+      if ( !currentState.IsLightOn && previousState.IsLightOn ) {
+        currentState.LastLightTurnedOffTime = currentState.CurrentTime;
+      }
+      if ( currentState.NumberOfPyroelectricSensorInterrupts != previousState.NumberOfPyroelectricSensorInterrupts ) {
+        currentState.LastDetectedHumanTime = currentState.CurrentTime;
+      }
+
+      return currentState;
+    }, initialState),
+  );
+
+stateStream.subscribe((state) => {
+  console.log("[Rx] stateStream:", state);
+});
+
+stateStream.subscribe((state) => {
+  const controlMessage = JSON.stringify({
+    Command: "SET_LED",
+    Color: (state.IsLightOn ? COLOR_BUSY : COLOR_FREE),
+  });
+  console.log("[MQTT] controlMessage:", controlMessage);
+  client.publish(CONTROL_TOPIC, controlMessage);
 });
