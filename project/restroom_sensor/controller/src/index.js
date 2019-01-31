3cc0165a6c8ffec6e557e29319c11465a86b963c
Yuya Kato <yuyakato@gmail.com>
2019-02-01 00:41:06 +0900

    制御電文を送信する処理を追加

--- project/restroom_sensor/controller/src/index.js
+++ project/restroom_sensor/controller/src/index.js
@@ -6,6 +6,8 @@ const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://127.0.0.1");
 
 const NOTIFICATION_TOPIC = "sensor/restroom/raw/notification";
+const CONTROL_TOPIC      = "sensor/restroom/raw/control";
+
 
 client.on("connect", () => {
   console.log("connect");
@@ -13,6 +15,20 @@ client.on("connect", () => {
   client.subscribe(NOTIFICATION_TOPIC, (err) => {
     console.log("subscribe");
   });
+
+  setInterval(() => {
+    const data = {
+      Command: "SET_LED",
+      Color: {
+        Red:   Math.floor(Math.random() * 255),
+        Green: Math.floor(Math.random() * 255),
+        Blue:  Math.floor(Math.random() * 255),
+      },
+    };
+    const message = JSON.stringify(data);
+    client.publish(CONTROL_TOPIC, message);
+    console.log("publish:", message);
+  }, 1000);
 });
 
 client.on("message", (topic, message) => {
