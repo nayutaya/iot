676041e9a30c3f545127d8fbf0f820250ad9a86f
Yuya Kato <yuyakato@gmail.com>
2019-02-01 10:24:57 +0900

    mosquittoサービスをバックグランドで起動するように変更

--- service/mosquitto/run_service.sh
+++ service/mosquitto/run_service.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eu
 source ./env.sh
-docker run --interactive --tty --rm \
+docker stop ${CONTAINER_NAME} || true
+docker rm   ${CONTAINER_NAME} || true
+docker run --detach \
+  --restart unless-stopped \
   --publish 1883:1883 \
   --publish 9001:9001 \
+  --name ${CONTAINER_NAME} \
   ${IMAGE_NAME}
