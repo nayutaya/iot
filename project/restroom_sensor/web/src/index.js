8dc10d09a3194482167202379e0d8c71897c55a1
Yuya Kato <yuyakato@gmail.com>
2019-02-02 13:59:16 +0900

    リファクタリング
    MQTTの処理をファイルに分割

--- project/restroom_sensor/web/src/index.js
+++ project/restroom_sensor/web/src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const express = require("express");
-const mqtt    = require("mqtt");
 const Rx      = require("rxjs");
 
 const MQTT_SERVER_URL = process.env.MQTT_SERVER_URL;
@@ -9,22 +8,11 @@ const STATE_TOPIC     = "sensor/restroom/state";
 console.log("MQTT_SERVER_URL:", MQTT_SERVER_URL);
 console.log("STATE_TOPIC:", STATE_TOPIC);
 
-const client = mqtt.connect(MQTT_SERVER_URL);
-
-client.on("connect", () => {
-  console.log("[MQTT] connect");
-
-  client.subscribe(STATE_TOPIC, (err) => {
-    console.log("[MQTT] subscribe:", STATE_TOPIC);
-  });
+const {mqttClient, stateSubject} = require("./mqtt")({
+  mqttServerUrl: MQTT_SERVER_URL,
+  stateTopic: STATE_TOPIC,
 });
 
-const stateSubject = new Rx.BehaviorSubject(null);
-client.on("message", (topic, message) => {
-  if ( topic === STATE_TOPIC ) {
-    stateSubject.next(JSON.parse(message));
-  }
-});
 stateSubject.subscribe((state) => {
   console.log("[Rx] state:", state);
 });
