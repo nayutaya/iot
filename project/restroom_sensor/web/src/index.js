6a156248c91d33ece1ee5a9edad45bc166ee8497
Yuya Kato <yuyakato@gmail.com>
2019-02-02 14:11:39 +0900

    状態履歴を生成するストリームを追加

--- project/restroom_sensor/web/src/index.js
+++ project/restroom_sensor/web/src/index.js
@@ -2,6 +2,11 @@
 
 const express = require("express");
 const Rx      = require("rxjs");
+const {
+  filter,
+  // map,
+  scan,
+} = require("rxjs/operators");
 
 const MQTT_SERVER_URL = process.env.MQTT_SERVER_URL;
 const STATE_TOPIC     = "sensor/restroom/state";
@@ -13,8 +18,24 @@ const {mqttClient, stateSubject} = require("./mqtt")({
   stateTopic: STATE_TOPIC,
 });
 
+const stateHistoryStream = stateSubject
+  .pipe(
+    filter((notificationMessage) => notificationMessage !== null),
+    scan((stateHistory, state) => {
+      const newStateHistory = [].concat(stateHistory);
+      newStateHistory.push(state);
+      // TODO: レコード数を制限する。
+      // TODO: レコードの期間を制限する。
+      return newStateHistory;
+    }),
+  )
+
 stateSubject.subscribe((state) => {
   console.log("[Rx] state:", state);
+});
+
+stateHistoryStream.subscribe((stateHistory) => {
+  console.log("[Rx] stateHistory:", stateHistory);
 });
 
 const app = express();
