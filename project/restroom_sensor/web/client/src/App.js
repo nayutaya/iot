fad9e8c6aa210ff88ba95966785262c17fa97396
Yuya Kato <yuyakato@gmail.com>
2019-02-04 21:16:01 +0900

    WebSocket経由でデータを取得する処理を追加

--- project/restroom_sensor/web/client/src/App.js
+++ project/restroom_sensor/web/client/src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import _ from "lodash";
+import Websocket from "react-websocket";
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateHistory: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  onMessage(data) {
+    const stateHistory =
+        _(this.state.stateHistory)
+          .concat(JSON.parse(data))
+          .sortBy((s) => s.CurrentTime)
+          .reverse()
+          .slice(0, 100)
+          .reverse()
+          .value();
+    // console.log("stateHistory:", stateHistory);
+    this.setState({stateHistory});
+  }
+
   render() {
     return (
       <div className="App">
         App
+        <Websocket url="ws://localhost:8080/" onMessage={(data) => this.onMessage(data)}/>
       </div>
     );
   }
