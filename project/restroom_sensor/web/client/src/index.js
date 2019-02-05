41fd72ff55f326cb0378ddd37d90b3738166df64
Yuya Kato <yuyakato@gmail.com>
2019-02-05 14:33:55 +0900

    環境変数REACT_APP_WS_URLを使ってWebSocketのURLを切り替えるように変更

--- project/restroom_sensor/web/client/src/index.js
+++ project/restroom_sensor/web/client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const wsUrl = "ws://localhost:8080/";
-// const wsUrl = window.location.href.replace("http://", "ws://");
+const REACT_APP_WS_URL = process.env.REACT_APP_WS_URL;
+const wsUrl = (REACT_APP_WS_URL != null ? REACT_APP_WS_URL : window.location.href.replace("http://", "ws://"));
 
 ReactDOM.render(<App wsUrl={wsUrl}/>, document.getElementById("root"));
