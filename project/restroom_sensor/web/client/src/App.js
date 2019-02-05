12b39818cfd3ccebfc2c27697df75c8f2a1b9ad9
Yuya Kato <yuyakato@gmail.com>
2019-02-05 12:14:41 +0900

    レイアウト、デザインを調整

--- project/restroom_sensor/web/client/src/App.js
+++ project/restroom_sensor/web/client/src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 
 import _ from "lodash";
+import moment from "moment";
 import WebSocket from "react-websocket";
 
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Collapse from "@material-ui/core/Collapse";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 
 export default class App extends Component {
   constructor(props) {
@@ -13,6 +22,7 @@ export default class App extends Component {
     this.state = {
       stateHistory: [],
       currentState: null,
+      expanded: false,
     };
   }
 
@@ -38,38 +48,73 @@ export default class App extends Component {
   }
 
   render() {
+    const formatTime = (time) => {
+      return moment(new Date(time)).format("YYYY/MM/DD HH:mm:ss");
+    };
+    const stateMessages = {
+      BUSY: "トイレは使用中です",
+      FREE: "トイレは空いています",
+      UNKNOWN: "トイレの照明が点きっぱなしかも？",
+    }
+    const stateTable = {
+      BUSY:    {color: "#ff9999", short: "使用中", message: "トイレは使用中です"},
+      FREE:    {color: "#b3ffb3", short: "空き",   message: "トイレは空いています"},
+      UNKNOWN: {color: "#ff9900", short: "不明",   message: "トイレの照明が点きっぱなしかも？"},
+    }
+    const getState = (state) => {
+      return (stateTable.hasOwnProperty(state) ? stateTable[state] : stateTable.UNKNOWN);
+    }
+    const {currentState} = this.state;
+
     return (
       <div className="App">
         <WebSocket url="ws://localhost:8080/" onMessage={(data) => this.onMessage(data)}/>
         <CssBaseline />
-        <Grid container spacing={16}>
-          <Grid item xs={6}>
-            {(this.state.currentState == null ? null :
-              <>
-                <div>CurrentTime: {this.state.currentState.CurrentTime}</div>
-                <div>State: {this.state.currentState.State}</div>
-              </>
-            )}
-          </Grid>
-          <Grid item xs={6}>
-            <table>
-              <thead>
-                <tr>
-                  <th>CurrentTime</th>
-                  <th>State</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.stateHistory.map((state) => {
-                  return (
-                    <tr key={state.CurrentTime}>
-                      <td>{state.CurrentTime}</td>
-                      <td>{state.State}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+
+        <Grid container direction="row" justify="center" alignItems="flex-start" spacing={16}>
+          <Grid item xs={4}>
+            <Card>
+              <CardHeader
+                  title="京都オフィス5階 トイレ"
+                  subheader={currentState == null ? "-" : formatTime(currentState.CurrentTime)} />
+              <CardContent>
+                {(currentState == null ? null :
+                  <Typography component="p">
+                    <div style={{backgroundColor: getState(currentState.State).color}}>状態: {getState(currentState.State).message}</div>
+                  </Typography>
+                )}
+              </CardContent>
+              <CardActions disableActionSpacing>
+                <IconButton
+                    onClick={() => this.setState({expanded: !this.state.expanded})}
+                    aria-expanded={this.state.expanded}
+                    aria-label="Show more">
+                  <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+              <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>日時</th>
+                        <th>状態</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.stateHistory.map((state) => {
+                        return (
+                          <tr key={state.CurrentTime} style={{backgroundColor: getState(state.State).color}}>
+                            <td>{formatTime(state.CurrentTime)}</td>
+                            <td>{getState(state.State).short}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </CardContent>
+              </Collapse>
+            </Card>
           </Grid>
         </Grid>
       </div>
