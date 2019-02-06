dba2952f8545cf8a6852581f837e21ec36ce99f9
Yuya Kato <yuyakato@gmail.com>
2019-02-06 17:44:48 +0900

    デバッグログの出力処理を削除

--- project/restroom_sensor/web/client/src/index.js
+++ project/restroom_sensor/web/client/src/index.js
@@ -8,6 +8,5 @@ const REACT_APP_LOCATION_NAME = process.env.REACT_APP_LOCATION_NAME
 
 const wsUrl        = (REACT_APP_WS_URL        != null && REACT_APP_WS_URL        !== "" ? REACT_APP_WS_URL        : window.location.href.replace("http://", "ws://"));
 const locationName = (REACT_APP_LOCATION_NAME != null && REACT_APP_LOCATION_NAME !== "" ? REACT_APP_LOCATION_NAME : "-");
-console.log("locationName:", locationName);
 
 ReactDOM.render(<App wsUrl={wsUrl} locationName={locationName} />, document.getElementById("root"));
