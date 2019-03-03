187e759a8d6885a7a8c1cfa7aff24b6c96b89310
Yuya Kato <yuyakato@gmail.com>
2019-03-04 00:35:08 +0900

    NeoPixelを点灯させる実験コードを追加

---
+++ sandbox/esp32/neopixel/src/main.cpp
@@ -0,0 +1,36 @@
+
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+
+constexpr uint8_t  kLedPin = 25;
+constexpr uint16_t kLedCount = 1;
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(kLedCount, kLedPin);
+RgbColor red(255, 0, 0);
+RgbColor green(0, 255, 0);
+RgbColor blue(0, 0, 255);
+RgbColor white(255);
+RgbColor black(0);
+uint8_t count = 0;
+
+void setup() {
+  pinMode(kLedPin, OUTPUT);
+
+  strip.Begin();
+  strip.Show();
+}
+
+void loop() {
+  switch ( count % 5 ) {
+    case 0: strip.SetPixelColor(0, red  ); break;
+    case 1: strip.SetPixelColor(0, green); break;
+    case 2: strip.SetPixelColor(0, blue ); break;
+    case 3: strip.SetPixelColor(0, white); break;
+    case 4: strip.SetPixelColor(0, black); break;
+  }
+  strip.Show();
+
+  count++;
+
+  delay(1000);  // [ms]
+}
