e87cbcb8d72bc4f57d9352da4bfe6a1bf200c42f
Yuya Kato <yuyakato@gmail.com>
2019-03-14 01:07:10 +0900

    カメラの初期化処理を追加

--- sandbox/esp32/camera/src/main.cpp
+++ sandbox/esp32/camera/src/main.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
-#include <WiFi.h>
 #include <ArduinoOTA.h>
+#include <esp_camera.h>
+#include <WiFi.h>
 
 extern const char *kWifiSsid;
 extern const char *kWifiPassword;
 
 // constexpr uint8_t kLedPin = 2;
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
 void setupSerial() {
   Serial.begin(115200);
@@ -52,6 +71,38 @@ void setupOta() {
 void setup() {
   setupSerial();
   setupOta();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size   = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count     = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+  Serial.printf("esp_camera_init: 0x%x", err);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
 }
 
 void handleOta() {
@@ -61,8 +112,7 @@ void handleOta() {
 void loop() {
   handleOta();
 
-  Serial.println("Hello world!");
-
+  // Serial.println("Hello world!");
   // digitalWrite(kLedPin, HIGH);
   // delay(1000);
   // digitalWrite(kLedPin, LOW);
