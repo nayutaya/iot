3bbd39f63eaf494f13ae289f3f83fd5df7a3e9d3
Yuya Kato <yuyakato@gmail.com>
2019-03-14 12:20:38 +0900

    リファクタリング

--- sandbox/esp32/camera/src/main.cpp
+++ sandbox/esp32/camera/src/main.cpp
@@ -8,23 +8,23 @@ extern const char *kWifiPassword;
 
 // constexpr uint8_t kLedPin = 2;
 
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+constexpr int8_t PWDN_GPIO_NUM   =  32;
+constexpr int8_t RESET_GPIO_NUM  =  -1;
+constexpr int8_t XCLK_GPIO_NUM   =   0;
+constexpr int8_t SIOD_GPIO_NUM   =  26;
+constexpr int8_t SIOC_GPIO_NUM   =  27;
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+constexpr int8_t Y9_GPIO_NUM     =  35;
+constexpr int8_t Y8_GPIO_NUM     =  34;
+constexpr int8_t Y7_GPIO_NUM     =  39;
+constexpr int8_t Y6_GPIO_NUM     =  36;
+constexpr int8_t Y5_GPIO_NUM     =  21;
+constexpr int8_t Y4_GPIO_NUM     =  19;
+constexpr int8_t Y3_GPIO_NUM     =  18;
+constexpr int8_t Y2_GPIO_NUM     =   5;
+constexpr int8_t VSYNC_GPIO_NUM  =  25;
+constexpr int8_t HREF_GPIO_NUM   =  23;
+constexpr int8_t PCLK_GPIO_NUM   =  22;
 
 void setupSerial() {
   Serial.begin(115200);
@@ -96,7 +96,7 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
   config.frame_size   = FRAMESIZE_SVGA;
-  config.jpeg_quality = 12;
+  config.jpeg_quality = 10;
   config.fb_count     = 1;
 
   esp_err_t err = esp_camera_init(&config);
