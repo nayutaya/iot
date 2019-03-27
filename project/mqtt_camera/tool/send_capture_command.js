1b9793b9da731b0c5bb1d1cfc196954846a3130b
Yuya Kato <yuyakato@gmail.com>
2019-03-28 00:41:39 +0900

    撮影要求電文をMQTTサーバに送信するツールを追加

---
+++ project/mqtt_camera/tool/send_capture_command.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const mqtt = require("mqtt");
+
+const MQTT_SERVER_URL = process.env.MQTT_SERVER_URL;
+const MQTT_TOPIC      = process.env.MQTT_SERVER_URL;
+console.log("MQTT_SERVER_URL:", MQTT_SERVER_URL);
+console.log("MQTT_TOPIC:", MQTT_TOPIC);
+
+const client = mqtt.connect(MQTT_SERVER_URL);
+
+client.on("connect", () => {
+  console.log("connect");
+
+  // TODO: 撮影要求電文を作成する。
+  const message = "{}";
+  client.publish(MQTT_TOPIC, message);
+  console.log("publish:", message);
+
+  client.end();
+});
